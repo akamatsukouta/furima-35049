49cb2c50d7b7d5f152698f712e7ed27b4e459016
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-04-01 17:51:13 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,14 @@ class User < ApplicationRecord
   has_many :purchase_manegements       
   
   
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
-  validates :family_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_day, presence: true
+   validates :nickname, presence: true
+   validates :email, presence: true
+ VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+   validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }, length: {minmum: 6}
+     
+   validates :family_name, presence: true
+   validates :family_name_kana, presence: true
+   validates :first_name, presence: true
+   validates :first_name_kana, presence: true
+   validates :birth_day, presence: true
 end
