87ecfd4fb13c4f49b2995e6a9c1125abf5706f1f
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-04-02 20:00:28 +0900

    ActiveStorageを導入、ActiveStorageを導入、バリデーション設定、商品出品ページ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
-  
   end
 
   def new
-    #@item = Item.new
+    @item = Item.new
+  end
+
+  def create
+    
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   #has_many :items
-  #has_many :purchase_manegements       
+  #has_many :purchase_manegements  
+   has_one_attached :image
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i
  with_options presence: true do
 
