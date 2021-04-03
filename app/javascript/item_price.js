a447b1e6454bdeccef24e151d0cc6382ba233f5d
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-04-03 23:22:13 +0900

    item_price.js修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,18 +1,15 @@
 window.addEventListener("load",()=>{
+    
 
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    console.log(inputValue);
-
-    const addTaxDom = document.getElementById("add-tax-price")
-    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-     console.log(addTaxDom);
-    
-     const profitNumber = document.getElementById("profit")
-     profitNumber.innerHTML = (Math.floor(parseInt(inputValue) - parseInt(addTaxDom.innerHTML)));
-       console.log(profitNumber);
+    const inputValue = priceInput.value; 
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue / 10);
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
+  
      
      
-  })
-})
+  });
+});
